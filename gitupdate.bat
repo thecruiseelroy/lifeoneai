a929751019a632bb46de4d9a8cf641a2309df1f5
Sean Craig <sean.m.craig02@gmail.com>
2026-01-30 07:52:29 -0700

    update0Fri001/30/2026007:52:29

--- gitupdate.bat
+++ gitupdate.bat
@@ -1,10 +1,6 @@
 @echo off
 title Git Update - Life One
 cd /d "%~dp0"
-set "DBG_LOG=%~dp0life-one-app\.cursor\debug.log"
-REM #region agent log
-echo ^{^"m^":^"start^",^"loc^":^"gitupdate.bat^",^"h^":^"H1^"^}>> "%DBG_LOG%"
-REM #endregion
 
 if exist "%~dp0deploy-env.bat" call "%~dp0deploy-env.bat"
 
@@ -22,9 +18,6 @@ if errorlevel 1 (
   goto :finish
 )
 cd ..
-REM #region agent log
-echo ^{^"m^":^"after_build^",^"loc^":^"gitupdate.bat^",^"h^":^"H2^"^}>> "%DBG_LOG%"
-REM #endregion
 
 if defined FTP_HOST (
   echo Deploying to Hostinger...
@@ -55,17 +48,8 @@ git status
 echo.
 
 REM Build commit message without dot (%%time%% can be "12:34:56.78" which breaks batch)
-REM #region agent log
-echo ^{^"m^":^"before_commit_msg^",^"loc^":^"gitupdate.bat^",^"h^":^"H5^"^}>> "%DBG_LOG%"
-REM #endregion
 set "COMMIT_MSG=update %date% %time:~0,8%"
-REM #region agent log
-echo ^{^"m^":^"after_set_raw^",^"loc^":^"gitupdate.bat^",^"h^":^"H5^"^}>> "%DBG_LOG%"
-REM #endregion
 set "COMMIT_MSG=%COMMIT_MSG: =0%"
-REM #region agent log
-echo ^{^"m^":^"before_git_commit^",^"loc^":^"gitupdate.bat^",^"h^":^"H1^"^}>> "%DBG_LOG%"
-REM #endregion
 git commit -m "%COMMIT_MSG%" 2>nul
 if errorlevel 1 (
   echo Nothing to commit or commit failed.
@@ -75,9 +59,6 @@ if errorlevel 1 (
 )
 
 :finish
-REM #region agent log
-echo ^{^"m^":^"finish^",^"loc^":^"gitupdate.bat^",^"h^":^"H4^"^}>> "%DBG_LOG%"
-REM #endregion
 echo.
 echo ----------------------------------------
 echo Finished. Press any key to close this window.
